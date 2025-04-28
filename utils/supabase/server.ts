93cca92cdef74060f28ee2ada9f0281601e52040
vahidkarimane <vahidkarimane@gmail.com>
2025-04-28 12:23:30 +1000

    fix build errors

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,7 +1,8 @@
 import {createServerClient, type CookieOptions} from "@supabase/ssr";
 import {cookies} from "next/headers";
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+export const createClient = async () => {
+	const cookieStore = await cookies();
 	return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
 		cookies: {
 			get(name: string) {
